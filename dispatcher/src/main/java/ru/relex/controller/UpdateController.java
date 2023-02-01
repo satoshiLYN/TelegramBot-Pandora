5c8b2b77fd427d2e07ccce534cf7a4bcf3d0ec3d
satoshiLYN <kcashflow@yandex.ru>
2023-02-01 23:53:29 +0300

    first commit. simple chatbot answers (doc, photo, unknown)

---
+++ dispatcher/src/main/java/ru/relex/controller/UpdateController.java
@@ -0,0 +1,86 @@
+package ru.relex.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.relex.service.UpdateProducer;
+import ru.relex.utils.MessageUtils;
+
+import static ru.relex.model.RabbitQueue.*;
+
+@Component
+@Log4j
+public class UpdateController {
+    private TelegramBot telegramBot;
+    private final MessageUtils messageUtils;
+    private final UpdateProducer updateProducer;
+
+    public UpdateController(MessageUtils messageUtils, UpdateProducer updateProducer) {
+        this.messageUtils = messageUtils;
+        this.updateProducer = updateProducer;
+    }
+
+
+    public void registerBot(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update){
+        if (update == null){
+            log.error("Полученный update равен null");
+            return;
+        }
+
+        if (update.getMessage() != null){
+            distributeMessagesByType(update);
+        } else {
+            log.error("Сообщение данного типа не распознано " + update);
+        }
+    }
+
+    private void distributeMessagesByType(Update update) {
+        var message = update.getMessage();
+        if (message.getText() != null){
+            processTextMessage(update);
+        } else if (message.getDocument() != null){
+            processDocMessage(update);
+        } else if (message.getPhoto() != null) {
+            processPhotoMessage(update);
+        } else {
+            setUnsupportedMessageTypeView(update);
+        }
+    }
+
+
+
+    private void setUnsupportedMessageTypeView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update, "Неподдерживаемый тип сообщения (ᗒᗣᗕ)՞");
+        setView(sendMessage);
+    }
+
+    private void setFileReceivedView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update, "Файл загружен (๑˃ᴗ˂)ﻭ");
+        setView(sendMessage);
+    }
+
+
+
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+
+    private void processPhotoMessage(Update update) {
+        updateProducer.produce(PHOTO_MESSAGE_UPDATE, update);
+        setFileReceivedView(update);
+    }
+
+    private void processDocMessage(Update update) {
+        updateProducer.produce(DOC_MESSAGE_UPDATE, update);
+        setFileReceivedView(update);
+    }
+
+    private void processTextMessage(Update update) {
+        updateProducer.produce(TEXT_MESSAGE_UPDATE, update);
+    }
+}
