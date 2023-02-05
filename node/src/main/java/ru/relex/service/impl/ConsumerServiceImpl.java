9743da4d4290ac2492835ef9017f5246672cf563
satoshiLYN <kcashflow@yandex.ru>
2023-02-05 23:57:06 +0300

    postgresql integration

--- node/src/main/java/ru/relex/service/impl/ConsumerServiceImpl.java
+++ node/src/main/java/ru/relex/service/impl/ConsumerServiceImpl.java
@@ -3,10 +3,9 @@ package ru.relex.service.impl;
 import lombok.extern.log4j.Log4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.relex.service.ConsumerService;
-import ru.relex.service.ProducerService;
+import ru.relex.service.MainService;
 
 import static ru.relex.model.RabbitQueue.*;
 
@@ -14,10 +13,10 @@ import static ru.relex.model.RabbitQueue.*;
 @Log4j
 public class ConsumerServiceImpl implements ConsumerService {
 
-    private final ProducerService producerService;
+    private final MainService mainService;
 
-    public ConsumerServiceImpl(ProducerService producerService) {
-        this.producerService = producerService;
+    public ConsumerServiceImpl(MainService mainService) {
+        this.mainService = mainService;
     }
 
 
@@ -25,12 +24,7 @@ public class ConsumerServiceImpl implements ConsumerService {
     @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
     public void consumeTextMessageUpdate(Update update) {
         log.debug("нода, заглушка, Text");
-
-        var message = update.getMessage();
-        var sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText("Hello from Node");
-        producerService.producerAnswer(sendMessage);
+        mainService.processTextMessage(update);
     }
 
     @Override
