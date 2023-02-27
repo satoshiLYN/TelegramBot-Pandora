1442dcd33c9e585c364dd5ab1cefec8021997fe8
satoshiLYN <kcashflow@yandex.ru>
2023-02-27 16:08:12 +0300

    added download doc and photo from telegram

--- node/src/main/java/ru/relex/service/impl/FileServiceImpl.java
+++ node/src/main/java/ru/relex/service/impl/FileServiceImpl.java
@@ -60,8 +60,9 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public AppPhoto processPhoto(Message telegramMessage) {
-        // TODO пока только одно фото, реализовать возможность обработки альбома
-        PhotoSize telegramPhoto = telegramMessage.getPhoto().get(0);
+        var photoSizeCount = telegramMessage.getPhoto().size();
+        var photoIndex = photoSizeCount > 1 ? telegramMessage.getPhoto().size() - 1 : 0;
+        PhotoSize telegramPhoto = telegramMessage.getPhoto().get(photoIndex);
         String fileId = telegramPhoto.getFileId();
         ResponseEntity<String> response = getFilePath(fileId);
         if (response.getStatusCode() == HttpStatus.OK) {
