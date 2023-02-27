0dfd635fafb3291c2a3653a789b2277daa2efcad
satoshiLYN <kcashflow@yandex.ru>
2023-02-27 17:52:11 +0300

    code review (deleting temporary files)

--- rest-service/src/main/java/ru/relex/service/FileService.java
+++ rest-service/src/main/java/ru/relex/service/FileService.java
@@ -8,5 +8,4 @@ import ru.relex.entity.BinaryContent;
 public interface FileService {
     AppDocument getDocument(String id);
     AppPhoto getPhoto(String id);
-    FileSystemResource getFileSystemResource(BinaryContent binaryContent);
 }
--- rest-service/src/main/java/ru/relex/service/impl/FileServiceImpl.java
+++ rest-service/src/main/java/ru/relex/service/impl/FileServiceImpl.java
@@ -1,19 +1,13 @@
 package ru.relex.service.impl;
 
 import lombok.extern.log4j.Log4j;
-import org.apache.commons.io.FileUtils;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import ru.relex.dao.AppDocumentDAO;
 import ru.relex.dao.AppPhotoDAO;
 import ru.relex.entity.AppDocument;
 import ru.relex.entity.AppPhoto;
-import ru.relex.entity.BinaryContent;
 import ru.relex.service.FileService;
 import ru.relex.utils.CryptoTool;
-
-import java.io.File;
-import java.io.IOException;
 
 @Log4j
 @Service
@@ -31,7 +25,7 @@ public class FileServiceImpl implements FileService {
     @Override
     public AppDocument getDocument(String hash) {
         var id = cryptoTool.idOf(hash);
-        if (id ==null) {
+        if (id == null) {
             return null;
         }
         return appDocumentDAO.findById(id).orElse(null);
@@ -40,22 +34,9 @@ public class FileServiceImpl implements FileService {
     @Override
     public AppPhoto getPhoto(String hash) {
         var id = cryptoTool.idOf(hash);
-        if (id ==null) {
+        if (id == null) {
             return null;
         }
         return appPhotoDAO.findById(id).orElse(null);
-    }
-
-    @Override
-    public FileSystemResource getFileSystemResource(BinaryContent binaryContent) {
-        try {
-            File temp = File.createTempFile("tempFile", ".bin");
-            temp.deleteOnExit();
-            FileUtils.writeByteArrayToFile(temp, binaryContent.getFileAsArrayOfBytes());
-            return new FileSystemResource(temp);
-        } catch (IOException e) {
-            log.error(e);
-            return null;
-        }
     }
 }
