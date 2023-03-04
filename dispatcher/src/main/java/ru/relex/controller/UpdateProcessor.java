a4b0fc28a3d82b3e4e58f0a0fccd220e65b02eb2
satoshiLYN <kcashflow@yandex.ru>
2023-03-04 17:16:14 +0300

    added webhook - final

--- dispatcher/src/main/java/ru/relex/controller/UpdateProcessor.java
+++ dispatcher/src/main/java/ru/relex/controller/UpdateProcessor.java
@@ -11,12 +11,12 @@ import static ru.relex.model.RabbitQueue.*;
 
 @Component
 @Log4j
-public class UpdateController {
+public class UpdateProcessor {
     private TelegramBot telegramBot;
     private final MessageUtils messageUtils;
     private final UpdateProducer updateProducer;
 
-    public UpdateController(MessageUtils messageUtils, UpdateProducer updateProducer) {
+    public UpdateProcessor(MessageUtils messageUtils, UpdateProducer updateProducer) {
         this.messageUtils = messageUtils;
         this.updateProducer = updateProducer;
     }
@@ -60,7 +60,7 @@ public class UpdateController {
     }
 
     private void setFileReceivedView(Update update) {
-        var sendMessage = messageUtils.generateSendMessageWithText(update, "Файл загружен (๑˃ᴗ˂)ﻭ");
+        var sendMessage = messageUtils.generateSendMessageWithText(update, "Файл загружается...");
         setView(sendMessage);
     }
 
